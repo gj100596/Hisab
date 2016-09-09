ac4b12a22c95f5061cf0c61a6324d638c3967ed4
GJ <gj100596@gmail.com>
2016-09-09 20:59:50 +0530

    feat: Added CardView Layout

--- app/src/main/java/gj/udacity/capstone/hisab/MainActivity.java
+++ app/src/main/java/gj/udacity/capstone/hisab/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
     @Override
