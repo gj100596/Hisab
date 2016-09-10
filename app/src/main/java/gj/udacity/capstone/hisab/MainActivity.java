3eb5c01256332b1f955700ed5b6e6ca863d4d5d3
GJ <gj100596@gmail.com>
2016-09-10 14:24:53 +0530

    feat: Dummy Recycler View

--- app/src/main/java/gj/udacity/capstone/hisab/MainActivity.java
+++ app/src/main/java/gj/udacity/capstone/hisab/MainActivity.java
@@ -5,9 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.feed,FeedFragment.newInstance())
+                .commit();
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
